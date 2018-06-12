430c677ec59efebed2099b88918bb01762b60525
kun.he <kun.he@tendcloud.com>
2018-06-12 14:41:10 +0800

    init

---
+++ src/main/java/com/hk/DiscardServerHandler.java
@@ -0,0 +1,9 @@
+package com.hk;
+
+/**
+ * Created by kunhe on 6/12/18.
+ */
+public class DiscardServerHandler {//extends ChannelInboundHandlerAdaptor {
+
+
+}
---
+++ src/main/java/com/hk/TheServer.java
@@ -0,0 +1,12 @@
+package com.hk;
+
+/**
+ * Created by kunhe on 6/12/18.
+ */
+public class TheServer {
+
+    public static void main(String [] args)
+    {
+
+    }
+}
